9140e01a991d03ac7e5ec78cdcc4f24f7dbd4749
Belac44 <belacc44@gmail.com>
2023-01-04 19:39:44 +0300

    Constructors

--- LearnKotlin/src/main/kotlin/Main.kt
+++ LearnKotlin/src/main/kotlin/Main.kt
@@ -1,25 +1,16 @@
 fun main() {
-    var belac = Person()
-    println("His age is ${belac.personAge}")
-    belac.setAge(21)
-    println("His age is ${belac.personAge}")
+    val person1 = Person("Belac")
 
-    val messi = Person()
-    val me = messi
-
-    println(messi.personAge)
-    println(me.personAge)
-
-    messi.setAge(35)
-    //me.setAge(21)
-
-    println(messi.personAge)
-    println(me.personAge)
+    person1.getName()
+    println(person1.name)
 }
 
-class Person {
-    var personAge = 0
-    fun setAge(age: Int){
-        personAge = age
+class Person(val name: String = "Unknown"){
+    fun getName(){
+        println("My name is $name")
+    }
+
+    override fun toString(): String {
+        return name
     }
 }
