b7237bac7847eac8a02e9bb9f8f4692fbe15406f
Belac44 <belacc44@gmail.com>
2023-01-04 18:45:50 +0300

    in keyword

--- LearnKotlin/src/main/kotlin/Main.kt
+++ LearnKotlin/src/main/kotlin/Main.kt
@@ -1,49 +1,13 @@
 fun main() {
-    val elements = listOf("A","B","C")
+    val number = -440
 
-    for(e in elements){
-        println("Current element: $e")
-    }
+    println(number in 0..100)
+    println('t' in "kotlin")
+    println('K' in "kotlin")
 
-    for (num in 1..7){
-        println("At number $num")
-    }
-
-    var range1 = 1..10
-    var range2 = 1 until 10
-
-    for (num in range1){
-        println("Range 1 numbers: $num")
-    }
-    for (num in range2){
-        println("Range 2 numbers: $num")
-    }
-
-    showRange(1..7)
-    showRange(5 downTo 1)
-    showRange(range=10 downTo 0 step 2)
-
-    for (letter in 'A'..'Z'){
-        print(letter)
-    }
-    println()
-
-    var character: Char = 'a'
-
-    println("Addition of the two gives: ${character+25}")
-    for (char in "Jnskhm "){
-            print(char+1)
-    }
-
-    repeat(3){
-        println("You never make a move when you are reeling from a loss")
-    }
+    println(isDigit('5'))
+    println(isNotDigit('a'))
 }
 
-fun showRange(range: IntProgression){
-    for (num in range){
-        println("Current number: $num")
-    }
-
-    println("// $range\n")
-}
+fun isDigit(ch: Char) = ch in '0'..'9'
+fun isNotDigit(ch: Char) = ch !in '0'..'9'
