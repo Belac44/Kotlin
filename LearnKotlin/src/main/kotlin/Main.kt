3ecbe38f5a144960cc965157677a245d78f6c296
Belac44 <belacc44@gmail.com>
2023-01-04 19:23:22 +0300

    class properties

--- LearnKotlin/src/main/kotlin/Main.kt
+++ LearnKotlin/src/main/kotlin/Main.kt
@@ -1,8 +1,25 @@
 fun main() {
-    val bel = House()
-    println(bel.name())
+    var belac = Person()
+    println("His age is ${belac.personAge}")
+    belac.setAge(21)
+    println("His age is ${belac.personAge}")
+
+    val messi = Person()
+    val me = messi
+
+    println(messi.personAge)
+    println(me.personAge)
+
+    messi.setAge(35)
+    //me.setAge(21)
+
+    println(messi.personAge)
+    println(me.personAge)
 }
 
-class House constructor(){
-    fun name() = "Entering the house.."
+class Person {
+    var personAge = 0
+    fun setAge(age: Int){
+        personAge = age
+    }
 }
